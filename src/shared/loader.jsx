a88d79054786a5b9ca946207e4f66ae5597025b4
Himanshi Khandelwal <himanshikhandelwal81@gmail.com>
2024-05-07 12:49:16 +0530

    Initial Commit

---
+++ src/shared/loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='load-bar'>
+      <bar></bar>
+      <bar></bar>
+      <bar></bar>
+    </div>
+  )
+}
+
+export default Loader
+
