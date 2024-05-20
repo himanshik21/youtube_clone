f456d6be8ca03b1fc14b78833b096c4cbb0af833
Himanshi Khandelwal <himanshikhandelwal81@gmail.com>
2024-05-20 18:47:17 +0530

    Fix useEffect dependencies and add babel plugin

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext} from "react";
 import { useParams } from "react-router-dom";
 
 import { fetchDataFromApi } from "../utils/api";
@@ -7,14 +7,16 @@ import LeftNav from "./LeftNav";
 import SearchResultVideoCard from "./SearchResultVideoCard";
 
 const SearchResult = () => {
+
    const [result, setResult] = useState();
    const { searchQuery } = useParams();
    const { setLoading } = useContext(Context);
 
+
    useEffect(() => {
      document.getElementById("root").classList.remove("custom-h");
      fetchSearchResults();
-   }, []);
+   }, [searchQuery]);
 
    const fetchSearchResults = () => {
      setLoading(true);
