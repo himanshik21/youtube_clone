8013624573b5582cdd1ab9cff889b5935eaa3bd5
Himanshi Khandelwal <himanshikhandelwal81@gmail.com>
2024-05-23 10:07:31 +0530

    commit

--- src/shared/VideoLength.jsx
+++ src/shared/VideoLength.jsx
@@ -1,16 +1,39 @@
+// import React from "react";
+// import moment from "moment";
+
+// const VideoLength = ({ time }) => {
+//   const videoLengthInSeconds = moment()
+//     ?.startOf("day")
+//     ?.seconds(time)
+//     ?.format("H:mm:ss");
+//   return (
+//     <span className="absolute bottom-2 right-2 bg-black py-1 px-2 text-white text-xs rounded-md">
+//       {videoLengthInSeconds}
+//     </span>
+//   );
+// };
+
+// export default VideoLength;
+
 import React from "react";
 import moment from "moment";
 
 const VideoLength = ({ time }) => {
-  const videoLengthInSeconds = moment()
-    ?.startOf("day")
-    ?.seconds(time)
-    ?.format("H:mm:ss");
+  let videoLengthInSeconds = moment();
+
+  if (time >= 3600) {
+    videoLengthInSeconds = moment().startOf("day").seconds(time).format("H:mm");
+  }
+  else{
+    videoLengthInSeconds = moment()
+    .startOf("day")
+    .seconds(time)
+    .format("mm:ss");
+  }
   return (
     <span className="absolute bottom-2 right-2 bg-black py-1 px-2 text-white text-xs rounded-md">
       {videoLengthInSeconds}
     </span>
   );
 };
-
 export default VideoLength;
