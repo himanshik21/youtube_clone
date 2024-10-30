f14bb4d6f1920862dc066faa49dc6d7b420f505d
Himanshi Khandelwal <himanshikhandelwal81@gmail.com>
2024-10-30 23:41:19 +0530

    update api limit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import Header from './components/Header';
 import Feed from './components/Feed';
 import SearchResult from './components/SearchResult';
